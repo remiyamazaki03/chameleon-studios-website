f8eab9d2c6a6fed97817f2549498ed84470e88d9
remiyamazaki03 <remiyamazaki@gmail.com>
2025-03-16 00:29:59 +0900

    migrate navbar from react vite project

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+import "../../globals.css";
+import styles from "./page.module.css";
+
+export default function Contact() {
+  return (
+    <div className="container2">
+      <main className={styles.main}>
+        <p>this is the contact page</p>
+      </main>
+    </div>
+  );
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,54 @@
+import Link from "next/link";
+
 export default function Navbar() {
+  const closeMenu = () => {
+    const navbar = document.getElementById("navbarNav");
+    if (navbar?.classList.contains("show")) {
+      navbar.classList.remove("show");
+    }
+  };
+
   return (
-    <div>
-      <p>Welcome to the navbar</p>
-    </div>
+    <nav className="navbar navbar-expand-md navbar-light fixed-top navbar-css">
+      <Link className="navbar-brand ms-3" href="/">
+        <img src="/logo-small.png" alt="Chameleon Studios logo" width={60} />
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav me-auto navul">
+          <li className="nav-item mx-0">
+            <Link className="nav-link" href="/about">
+              About
+            </Link>
+          </li>
+
+          <li className="nav-item">
+            <Link className="nav-link" href="/voicecoach">
+              Private voice lessons
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" href="/webdeveloper">
+              Website development
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" href="/contact">
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
