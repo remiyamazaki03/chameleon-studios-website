6c08f52eceb25264ded90a21c4f594f2fa356679
remiyamazaki03 <remiyamazaki@gmail.com>
2026-02-01 21:27:08 +0900

    embed recital video on Events page

--- src/app/[lang]/events/page.tsx
+++ src/app/[lang]/events/page.tsx
@@ -8,26 +8,23 @@ export default function Events() {
       <h2 className="title mt-2 mb-0 d-flex align-items-center">
         {t("Events.top")}
       </h2>
-      <div className={`container1 ps-5 text-center ${styles.desc}`}>
-        <p className="mt-4" style={{ whiteSpace: "pre-line" }}>
+      <div className={`container1 text-center ${styles.desc}`}>
+        <p className="mt-md-5 px-4" style={{ whiteSpace: "pre-line" }}>
           {t("Events.p1")}
         </p>
-        <p>{t("Events.p2")}</p>
+        <p className="mb-5 px-4">{t("Events.p2")}</p>
 
-        <p>
-          {t.rich("Events.cta", {
-            youtube: (chunks) => (
-              <a
-                href="https://www.youtube.com/watch?v=NttEhG1qiQs"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="brand"
-              >
-                {chunks}
-              </a>
-            ),
-          })}
-        </p>
+        <div className={`mx-auto mb-4 ${styles.video}`}>
+          <div className="ratio ratio-16x9">
+            <iframe
+              title="Chameleon Studios Recital Highlights video"
+              src="https://www.youtube.com/embed/NttEhG1qiQs"
+              width="300"
+              height="200"
+              allowFullScreen
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
