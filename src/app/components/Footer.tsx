11969846a4b18364a4dcf269f3f90b05eec2d619
remiyamazaki03 <remiyamazaki@gmail.com>
2026-02-21 20:56:40 +0900

    edit footer content & clean up code

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,134 +5,63 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faInstagram,
   faYoutube,
-  faXTwitter,
   faLinkedin,
-  faGithub,
 } from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer({ lang }: { lang: string }) {
   const t = useTranslations("Footer");
+  const socials = [
+    {
+      href: "https://www.instagram.com/remi.0.3",
+      icon: faInstagram,
+      className: styles.instLogo,
+    },
+    {
+      href: "https://www.youtube.com/@remi03",
+      icon: faYoutube,
+      className: styles.youtubeLogo,
+    },
+    {
+      href: "https://www.linkedin.com/in/remi-yamazaki-77580b55/",
+      icon: faLinkedin,
+      className: styles.linkedinLogo,
+    },
+  ];
+  type SocialLinksProps = {
+    className?: string;
+  };
+  const SocialLinks = ({ className = "" }: SocialLinksProps) => (
+    <div className={`d-flex ${className}`}>
+      {socials.map(({ href, icon, className: iconClass }, i) => (
+        <a
+          key={i}
+          href={href}
+          target="_blank"
+          rel="noreferrer"
+          className="fs-1 mx-3"
+        >
+          <FontAwesomeIcon icon={icon} className={iconClass} />
+        </a>
+      ))}
+    </div>
+  );
   return (
     <div>
       <div className={`row ${styles.contact}`}>
-        <div className="col-md-6 d-none d-md-block">
-          <h1 className="ps-5 mt-5 mb-2 mb-md-5 homepage">
-            <div className="brand">
-              <div>{t("Footer.cta1")}</div>
-              {t("Footer.cta2")}
-            </div>
+        <div className="col-md-6">
+          <h1 className="mt-5 mb-2 mb-md-5 ps-md-5 d-flex justify-content-center justify-content-md-start">
+            <div className={`brand ${styles.cta}`}>{t("cta")}</div>
           </h1>
-
-          <div className="d-flex ps-5">
-            <a
-              href="https://www.instagram.com/remi.0.3"
-              className="fs-1 me-3"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FontAwesomeIcon
-                icon={faInstagram}
-                className={`${styles.instLogo}`}
-              />
-            </a>
-            <a
-              href="https://www.youtube.com/@remi03"
-              className="fs-1 mx-3"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FontAwesomeIcon
-                icon={faYoutube}
-                className={`${styles.youtubeLogo}`}
-              />
-            </a>
-            <a
-              href="https://twitter.com/03_remi"
-              className="fs-1 mx-3"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FontAwesomeIcon
-                icon={faXTwitter}
-                className={`${styles.twitterLogo}`}
-              />
-            </a>
-            <a
-              href="https://www.linkedin.com/in/remi-yamazaki-77580b55/"
-              className="fs-1 mx-3"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FontAwesomeIcon
-                icon={faLinkedin}
-                className={`${styles.linkedinLogo}`}
-              />
-            </a>
-            <a
-              href="https://github.com/remiyamazaki03"
-              className="fs-1 ms-3"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FontAwesomeIcon
-                icon={faGithub}
-                className={`${styles.githubLogo}`}
-              />
-            </a>
+          <div className="ps-4">
+            <SocialLinks className="justify-content-center justify-content-md-start" />
           </div>
-          <p className={`mt-5 mb-2 mb-md-5 ps-5 ${styles.email}`}>
+          <p
+            className={`mt-5 mb-2 mb-md-5 ps-md-5 d-flex justify-content-center justify-content-md-start ${styles.email}`}
+          >
             hello@chameleon-studios.com
           </p>
         </div>
         <div className="col-md-6 d-flex flex-column">
-          <h2 className="d-block d-md-none text-center my-4 brand fs-1">
-            {t("Footer.ctas")}
-          </h2>
-          <div className="d-flex d-block d-md-none mb-3 justify-content-center">
-            <a
-              href="https://www.instagram.com/remi.0.3"
-              className="fs-1 me-3"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i className="fa-brands fa-instagram inst-logo"></i>
-            </a>
-            <a
-              href="https://www.youtube.com/@remi03"
-              className="fs-1 mx-3"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i className="fa-brands fa-youtube youtube-logo"></i>
-            </a>
-            <a
-              href="https://twitter.com/03_remi"
-              className="fs-1 mx-3"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i className="fa-brands fa-x-twitter twitter-logo"></i>
-            </a>
-            <a
-              href="https://www.linkedin.com/in/remi-yamazaki-77580b55/"
-              className="fs-1 mx-3"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i className="fa-brands fa-linkedin linkedin-logo"></i>
-            </a>
-            <a
-              href="https://github.com/remiyamazaki03"
-              className="fs-1 ms-3"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i className="fa-brands fa-github github-logo"></i>
-            </a>
-          </div>
-          <div className="d-flex d-block d-md-none mb-3 justify-content-center">
-            email: hello@chameleon-studios.com
-          </div>
           <div
             className="me-sm-5 d-flex align-items-center justify-content-center"
             style={{ height: "100vh", width: "100%" }}
