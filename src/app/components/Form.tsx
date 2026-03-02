be05d5e9aaeade10fd4001125cc7607bab2ca7b5
remiyamazaki03 <remiyamazaki@gmail.com>
2026-03-03 01:15:19 +0900

    re-insert netlify form with hidden html form

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -1,12 +1,58 @@
+"use client";
+
 export default function Form() {
   return (
     <div>
-      <div id="ff-compose"></div>
-      <script
-        async
-        defer
-        src="https://formfacade.com/include/117331806213236142768/form/1FAIpQLScUhh9xuSczMLVhG_G-fg1RjpPgJVIlVYnGsAV-9gzZ1vrJiA/classic.js?div=ff-compose"
-      ></script>
+      <form
+        name="Contact Form"
+        method="POST"
+        action="/"
+        data-netlify="true"
+        netlify-honeypot="bot-field"
+        className="w-100"
+      >
+        <p hidden>
+          <label>
+            Don’t fill this out: <input name="bot-field" type="text" />
+          </label>
+        </p>
+
+        <div className="mb-3">
+          <label>Full name・フルネーム </label>
+          <input type="text" name="name" required className="form-control" />
+        </div>
+
+        <div className="mb-3">
+          <label>Email・メールアドレス</label>
+          <input type="email" name="email" required className="form-control" />
+        </div>
+
+        <div className="mb-3">
+          <label>Confirm Email・メールアドレス確認</label>
+          <input
+            type="email"
+            name="confirm-email"
+            required
+            className="form-control"
+          />
+        </div>
+
+        <div className="mb-3">
+          <label>
+            Location (e.g. Tokyo, Japan / CA, USA)・お住まい（例：札幌、東京）
+          </label>
+          <input type="text" name="location" className="form-control" />
+        </div>
+
+        <div className="mb-3">
+          <label>Message・お問合せ内容</label>
+          <textarea name="message" required className="form-control" rows={4} />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Send
+        </button>
+      </form>
     </div>
   );
 }
